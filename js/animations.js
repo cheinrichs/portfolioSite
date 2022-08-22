c0f2842970a0980f0f048bf4c7e7dc3446231089
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2022-08-22 16:50:22 -0600

    jquery fix

--- js/animations.js
+++ js/animations.js
@@ -2,35 +2,35 @@ jQuery(function () {
 
   $('#scrollDown1').delay( 500 ).fadeIn('slow ');
 
-  $('#scrollDown1').click( function(){
+  $('#scrollDown1').on('click', function(){
     $('#section2').ScrollTo({
       duration: 1500,
       easing: 'swing'
     });
   });
 
-  $('#scrollDown2').click( function(){
+  $('#scrollDown2').on('click', function(){
     $('#section3').ScrollTo({
       duration: 1500,
       easing: 'swing'
     });
   });
 
-  $('#scrollDown3').click( function(){
+  $('#scrollDown3').on('click', function(){
     $('#section4').ScrollTo({
       duration: 1500,
       easing: 'swing'
     });
   });
 
-  $('#scrollDown4').click( function(){
+  $('#scrollDown4').on('click', function(){
     $('#section5').ScrollTo({
       duration: 1500,
       easing: 'swing'
     });
   });
 
-  $('#scrollDown5').click( function(){
+  $('#scrollDown5').on('click', function(){
     $('#section6').ScrollTo({
       duration: 1500,
       easing: 'swing'
